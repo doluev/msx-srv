679a88c491d80123656772b292902853cc18a805
doluev <140840005+doluev@users.noreply.github.com>
2025-08-27 09:54:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ console.log('Starting server...');
 const startData = {
   "name": "MSX Player",
   "version": "1.0.0",
-  "parameter": "https://msx-srv.onrender.com/msx/menu.json"
+  "parameter": "menu:https://msx-srv.onrender.com/msx/menu.json"
 };
 
 const menuData = {
